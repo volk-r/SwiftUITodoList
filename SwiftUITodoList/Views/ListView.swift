945b2ad8bff45df777eba0a72bdf351642ecf7f1
RRomanov <RRomanov@awardwallet.com>
2024-01-20 20:25:05 +0500

    Todo List #1 Create a List of Todo items in SwiftUI

---
+++ SwiftUITodoList/Views/ListView.swift
@@ -0,0 +1,18 @@
+//
+//  ListView.swift
+//  SwiftUITodoList
+//
+//  Created by Roman Romanov on 20.01.2024.
+//
+
+import SwiftUI
+
+struct ListView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    ListView()
+}
