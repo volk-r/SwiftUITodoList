10156e4ecf4efe18521701206c1e1678deb5c49c
RRomanov <RRomanov@awardwallet.com>
2024-01-20 18:30:31 +0500

    Initial Commit

---
+++ SwiftUITodoList/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  SwiftUITodoList
+//
+//  Created by Roman Romanov on 20.01.2024.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ SwiftUITodoList/SwiftUITodoListApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUITodoListApp.swift
+//  SwiftUITodoList
+//
+//  Created by Roman Romanov on 20.01.2024.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUITodoListApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
