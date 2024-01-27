ee21f1c5271e968838d23243566c6ba866ac4269
Roman Romanov <rromanov.vk@gmail.com>
2024-01-27 20:04:49 +0500

    Todo List #6 Adapt SwiftUI app for iPad, Landscape, and Dark Mode

--- SwiftUITodoList/Views/AddView.swift
+++ SwiftUITodoList/Views/AddView.swift
@@ -12,7 +12,6 @@ struct AddView: View {
     @Environment(\.dismiss) var dismiss
     @EnvironmentObject var listViewModel: ListViewModel
     @State var textFieldText: String = ""
-    @State var textFieldColor: Color = Color(#colorLiteral(red: 0.921431005, green: 0.9214526415, blue: 0.9214410186, alpha: 1))
     
     @State var alertTitle: String = ""
     @State var showAlert: Bool = false
@@ -23,7 +22,7 @@ struct AddView: View {
                 TextField("Type something here...", text: $textFieldText)
                     .padding(.horizontal)
                     .frame(height: 55)
-                    .background(textFieldColor)
+                    .background(Color(UIColor.secondarySystemBackground))
                     .clipShape(RoundedRectangle(cornerRadius: 10))
                 
                 Button(action: saveButtonPressed, label: {
